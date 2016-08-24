c242a6d9ae10ce033d077b77ab419b0d2f05301c
Miqueias Penha <miqueiaspenha@gmail.com>
2016-08-23 22:20:04 -0300

    S3A29

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 # Create your views here.
 
 def index(request):
@@ -21,8 +22,16 @@ def index(request):
 
 def details(request, slug):
 	course = get_object_or_404(Course, slug=slug)
-	context = {
-		'course': course
-	}
+	context = {}
+	if request.method == "POST":
+		form = ContactCourse(request.POST)
+		if form.is_valid():
+			context['is_valid'] = True
+			print(form.cleaned_data)
+			form = ContactCourse()
+	else:
+		form = ContactCourse()
+	context['course'] = course
+	context['form'] =  form
 	template_name = 'courses/details.html'
 	return render(request, template_name, context)
--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
+
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "Nome <miqueiaspenha@gmail.com>"
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "miqueiaspenha@gmail.com"
+EMAIL_HOST_PASSWORD = "senha"
+EMAIL_PORT = 587
