041bc1bbddc256f5893800ced2af98fc5be9d78c
Miqueias Penha <miqueiaspenha@gmail.com>
2016-08-29 21:12:43 -0300

    S4A45

--- simplemooc/accounts/forms.py
+++ simplemooc/accounts/forms.py
@@ -1,6 +1,35 @@
-form django import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RegisterForm(UserCreationForm):
 
-    emial = forms.EmailField(label='E-mail')
+    email = forms.EmailField(label='E-mail')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Já existe um usuário com este E-mail")
+        return email
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class EditAccountForm(forms.ModelForm):
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        queryset = User.objects.filter(email=email).exclude(pk=self.instance.pk)
+        if queryset.exists():
+            raise forms.ValidationError("Já existe um usuário com este E-mail")
+        return email
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -3,6 +3,10 @@ from django.contrib.auth import views
 from simplemooc.accounts import views as accounts_view
 
 urlpatterns = [
+	url(r'^$', accounts_view.dashboard, name='dashboard'),
 	url(r'^entrar/$', views.login, {'template_name': 'accounts/login.html'}, name='login'),
 	url(r'^registro/$', accounts_view.register, name='register'),
+	url(r'^sair/$', views.logout, {'next_page': 'core:home'}, name='logout'),
+	url(r'^editar/$', accounts_view.edit, name='edit'),
+	url(r'^editar-senha/$', accounts_view.edit_password, name='edit_password'),
 ]
