413a6b7e0dc0c627fc7e20cc1fa0a13efb0ba07b
Miqueias Penha <miqueiaspenha@gmail.com>
2016-12-04 17:53:27 -0300

    alterando o admin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -4,15 +4,26 @@ from django.contrib import admin
 
 from .models import (Course, Enrollment, Announcement, Comment, Lesson, Material)
 
-class CourseAdmin(admin.ModelAdmin):
-
-	list_display = ['name', 'slug', 'start_date', 'create_at']
-	search_fields = ['name', 'slug']
-	prepopulated_fields = {'slug': ('name',)}
+class CommentInlineAdmin(admin.StackedInline):
+	
+	model = Comment
 
 class MaterialInlineAdmin(admin.StackedInline):
 
 	model = Material
+
+class EnrollmentInlineAdmin(admin.StackedInline):
+	model = Enrollment
+	exclude = ['status']
+
+class AnnouncementAdmin(admin.ModelAdmin):
+	list_display = ['title', 'content']
+	search_fields = ['title']
+	list_filter = ['created_at']
+
+	inlines = [
+		CommentInlineAdmin
+	]
 
 class LessonAdmin(admin.ModelAdmin):
 
@@ -24,6 +35,16 @@ class LessonAdmin(admin.ModelAdmin):
 		MaterialInlineAdmin
 	]
 
+class CourseAdmin(admin.ModelAdmin):
+
+	list_display = ['name', 'slug', 'start_date', 'create_at']
+	search_fields = ['name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
+
+	inlines = [
+		EnrollmentInlineAdmin
+	]
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register([Enrollment, Announcement, Comment, Material])
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Announcement, AnnouncementAdmin)
